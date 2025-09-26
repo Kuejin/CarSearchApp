a84bfa88c1f70005f067b6c94c3980fe4d1bd6ac
Kuejin Yeon <ulbobae9739@naver.com>
2025-09-26 17:12:52 +0900

    3

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+// db.js
 require('dotenv').config();
 const mysql = require('mysql2');
 
@@ -22,9 +23,9 @@ const pool = mysql.createPool({
   port: dbUrl.port || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  ssl: { rejectUnauthorized: false },
 });
 
+// 연결 테스트
 pool.getConnection((err, conn) => {
   if (err) {
     console.error('❌ DB 연결 실패:', err.message);
